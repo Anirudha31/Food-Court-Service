b8fe8d0296e51777822a240dfd8c87f344e0031e
Rana2 <ranapratapchaulya2@gmail.com>
2026-01-28 14:33:35 +0530

    fix

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 
 // Security middleware
 app.use(helmet({
-  contentSecurityPolicy: false, // Disable CSP for development
+  contentSecurityPolicy: false, 
 }));
 app.use(cors({
   origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
